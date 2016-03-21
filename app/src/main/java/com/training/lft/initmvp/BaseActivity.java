2ad3d78b0228617d76db68bb5a78f77f80c805c9
laaptu <laaptu@laaptus-MacBook-Pro.local>
2016-03-21 15:29:55 +0545

    Adding layout and views to Login

--- app/src/main/java/com/training/lft/initmvp/BaseActivity.java
+++ app/src/main/java/com/training/lft/initmvp/BaseActivity.java
@@ -1,7 +1,22 @@
-package com.training.lft.initmvp.login;
+package com.training.lft.initmvp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by laaptu on 3/21/16.
  */
-public class BaseActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+    protected abstract int getLayoutId();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+
+    }
 }
--- app/src/main/java/com/training/lft/initmvp/login/LoginActivity.java
+++ app/src/main/java/com/training/lft/initmvp/login/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.training.lft.initmvp.login;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.training.lft.initmvp.BaseActivity;
 import com.training.lft.initmvp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
     }
 }
