6b9e1494aed7e896e054974612474be15043cdc9
laaptu <laaptu@laaptus-MacBook-Pro.local>
2016-03-24 09:16:20 +0545

    Adding some interactor logic

--- app/src/main/java/com/training/lft/initmvp/login/LoginInteractorImpl.java
+++ app/src/main/java/com/training/lft/initmvp/login/LoginInteractorImpl.java
@@ -1,12 +1,31 @@
 package com.training.lft.initmvp.login;
 
+import com.training.lft.initmvp.utils.PrefHelper;
+import com.training.lft.initmvp.utils.RestHelper;
+import com.training.lft.initmvp.utils.Status;
+import com.training.lft.initmvp.utils.StatusCallback;
+
 /**
  * Created by laaptu on 3/23/16.
  */
-public class LoginInteractorImpl implements LoginInteractor {
+public class LoginInteractorImpl implements LoginInteractor, StatusCallback {
     @Override
     public void login(String uname, String pwd, OnLoginFinishedListener listener) {
 
+    }
+
+    private void makeLoginUsingDb(String uname, String pwd) {
+        PrefHelper prefHelper = PrefHelper.getInstance();
+        prefHelper.loginOrRegister(uname, pwd, this);
+    }
+
+    private void makeLoginUsingRestAPI(String uname, String pwd) {
+        RestHelper restHelper = RestHelper.getInstance();
+        restHelper.loginOrRegister(uname,pwd,this);
+    }
+
+    @Override
+    public void onResult(Status status) {
 
     }
 }
